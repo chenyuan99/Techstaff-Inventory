f980d70e9a904d1c0f625e11c9abd11b270e9b0e
Yuan Chen <18861720589@163.com>
2021-02-03 13:13:21 -0500

    update index page

--- hello/admin.py
+++ hello/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import JobApplication, JobUpdate
+from .models import *
 
 class JobUpdateInline(admin.StackedInline):
     model = JobUpdate
