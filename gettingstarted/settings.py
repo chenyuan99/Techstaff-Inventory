b596b2a8c3a8184c14cfe1e9ccea2d8997023b83
Yuan Chen <18861720589@163.com>
2021-03-19 15:12:01 -0400

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "import_export",
     "django_filters",
     'crispy_forms',
     "hello",
