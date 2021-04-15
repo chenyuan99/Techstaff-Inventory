45a543d404e1ebe880d35ff62495580a01970760
Yuan Chen <18861720589@163.com>
2021-04-15 16:51:41 -0400

    Update resources.py

--- hello/resources.py
+++ hello/resources.py
@@ -4,3 +4,23 @@ from hello.models import *
 class DeviceResource(resources.ModelResource):
     class Meta:
         model = Device
+
+class FacultyResource(resources.ModelResource):
+    class Meta:
+        model = Faculty
+
+class BuildingResource(resources.ModelResource):
+    class Meta:
+        model = Building
+
+class NetworkInterfaceResource(resources.ModelResource):
+    class Meta:
+        model = NetworkInterface
+
+class DeviceResource(resources.ModelResource):
+    class Meta:
+        model = Device
+
+class IPAddrResource(resources.ModelResource):
+    class Meta:
+        model = IPAddr
